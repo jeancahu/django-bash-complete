60fec376f4a546af6263c42e5c08e036dadec487
Jeancahu <jeancahu@gmail.com>
2021-11-12 18:53:28 -0600

    add: complete dynamic

--- manage.py.completion.sh
+++ manage.py.completion.sh
@@ -1,6 +1,29 @@
-for COMMAND in ./manage.py django-admin
-do
 
-    complete -W "changepassword createsuperuser remove_stale_contenttypes check compilemessages createcachetable dbshell diffsettings dumpdata flush inspectdb loaddata makemessages makemigrations migrate sendtestemail shell showmigrations sqlflush sqlmigrate sqlsequencereset squashmigrations startapp startproject test testserver clearsessions collectstatic findstatic runserver" $COMMAND
+function _complete_django_admin () {
+    if (( ${#COMP_WORDS[@]} > 2 ))
+    then
+        return
+    fi
 
-done
+    SUBCOMMANDS="changepassword createsuperuser remove_stale_contenttypes check compilemessages createcachetable dbshell diffsettings dumpdata flush inspectdb loaddata makemessages makemigrations migrate sendtestemail shell showmigrations sqlflush sqlmigrate sqlsequencereset squashmigrations startapp startproject test testserver clearsessions collectstatic findstatic runserver"
+
+    COMPREPLY=(
+        $(compgen -W "$SUBCOMMANDS" "${COMP_WORDS[1]}")
+              )
+}
+
+function _complete_django_manage () {
+    if (( ${#COMP_WORDS[@]} > 2 ))
+    then
+        return
+    fi
+
+    SUBCOMMANDS="changepassword createsuperuser remove_stale_contenttypes check compilemessages createcachetable dbshell diffsettings dumpdata flush inspectdb loaddata makemessages makemigrations migrate sendtestemail shell showmigrations sqlflush sqlmigrate sqlsequencereset squashmigrations startapp startproject test testserver clearsessions collectstatic findstatic runserver"
+
+    COMPREPLY=(
+        $(compgen -W "$SUBCOMMANDS" "${COMP_WORDS[1]}")
+    )
+}
+
+complete -F _complete_django_manage ./manage.py
+complete -F _complete_django_admin django-admin
